647adc8ca9be576ff0e06eb3cec7b325f00c7405
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-08-27 20:38:42 -0400

    update

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 
-let text = "Thanks for watching, have a good day"
+let text = "Thanks for Watching, have a good day";
 let i = 0;
 
 function writeText(){
     document.body.innerText = text.slice(0,i);
-    i++
-    if( i> text.lenght){
+    i++;
+    if(i>text.length){
         i = 0;
     }
 }
